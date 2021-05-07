be96969e9ccd3767e2019e190140e611c14dfcfd
Amine Chabane <73701609+MrAragornX5@users.noreply.github.com>
2021-05-07 04:13:16 +0200

    Update MCMC_metro.py

--- MCMC_metro.py
+++ MCMC_metro.py
@@ -1,38 +1,69 @@
+import numpy, time
 
-import numpy, random, datetime
+def PDF_gauss(x):
+    return 0.5*numpy.sqrt(numpy.pi)*numpy.exp(-0.5*numpy.power(x, 2))
 
 
-
-def metropolis(x_old, a):
-    random.seed(datetime.datetime.now())
+def metropolis(x_start, a, N):
     
-    delta = numpy.random.uniform(-1, 1) 
+    numpy.random.seed(int(time.time()))
+    markov_chain = numpy.zeros(N)
     
-    x_update = x_old + a*delta
+    accept, reject = 0, 0
     
-    R = numpy.exp(-0.5*(x_update**2 - x_old**2))
+    x_old = x_start
+    markov_chain[0] = x_old
     
-    if R >= 1:
-        x_old = x_update 
-        print("R>=1")
-        return 1 # accept
-    elif 0 < R < 1:
-        print("0<R<1")
-        x_old = x_update
-        return 1
-    else:
-        return 0 # reject
+    for i in range(0, N):
+        delta = numpy.random.uniform(-1, 1)
+        x_new = x_old + a*delta
+        
+        R = PDF_gauss(x_new)/PDF_gauss(x_old)
+        
+        if R >= 1:
+            markov_chain[i] = x_new
+            accept+=1
+        else:
+            x_ran = numpy.random.uniform(0, 1)
+            if x_ran < R:
+                markov_chain[i] = x_new
+                accept+=1   
+            else:
+                markov_chain[i] = markov_chain[i-1]
+                reject+=1
+    
+    return markov_chain
 
-accept = 0
-N = 10
-x_start = 0
-a = 3
-
-for i in range(N):
-    accept += metropolis(x_start, a)
+def calc_mean(data_array, N):
+    mean = 0
     
-    print("\n\n\n\nN=", i)
-    
-accept/=N
+    for i in range(N):
+        mean += data_array[i]/N
+        
+    return mean
 
-print(accept)
+def naive_error(data_array, N):
+    sigma_0, sigma_naive = 0.0, 0.0
+    mean = calc_mean(data_array, N)
+    
+    for i in range(N):
+        sigma_0 += numpy.power(abs(data_array[i]-mean), 2)
+        sigma_0 /= (N-1)
+        sigma_naive = numpy.sqrt(sigma_0/N)
+ 
+    return sigma_naive
+
+#class DataSet:
+#    def __init__(self, N, data_array):
+#        self.N = N
+#        self.data_array = numpy.zeros(self.N)
+#        
+#        
+#    def print_data(self):
+#        pass
+#    
+#    def calc_mean(self):
+#        pass
+#    def clac_error(self):
+#        pass
+    
